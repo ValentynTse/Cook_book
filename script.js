040340cddad9aff948a0a1335df69134ab8ebc40
ValentynTse <tsehichko@gmail.com>
2024-01-19 13:59:29 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,38 @@
+function app() {
+   const form = document.querySelector('form');
+   const input = document.querySelector('input');
+   const wrapper = document.querySelector('.results-wrapper')
+   const renderItem = (itemData) => {
+      return `
+         <div class="item-wrapper">
+         <img src="${itemData.strMealThumb}" />
+         <h3>${itemData.strMeal}</h3>
+         <p>${itemData.strCategory}</p>
+         <p>${itemData.strArea}</p>
+         <p>${itemData.strInstructions}</p>
+         </div>
+         `;
+   };
+   form.addEventListener('submit', (e) => {
+      e.preventDefault();
+      console.log('Form submitted');
+      console.log(input.value);
+      fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=' + input.value)
+         .then((response) => {
+            return response.json();
+         })
+         .then((data) => {
+            let content = '';
+            for (let meal of data.meals) {
+               content += renderItem(meal);
+            }
+            console.log(data);
+            wrapper.innerHTML = content;
+         })
+         .catch(err => {
+            console.error(err);
+            alert('Something went wrong!')
+         })
+   });
+}
+app();
