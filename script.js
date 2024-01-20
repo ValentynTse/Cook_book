c5b4245f52ad5c6a51687241a18e0a08a04e16fb
ValentynTse <tsehichko@gmail.com>
2024-01-20 14:04:21 +0200

    adds timeout

--- script.js
+++ script.js
@@ -13,26 +13,38 @@ function app() {
          </div>
          `;
    };
+
+   const showLoading = () => {
+      wrapper.innerHTML = '<p>Loading...</p>';
+   };
+   const hideLoading = () => {
+      wrapper.innerHTML = '';
+   };
    form.addEventListener('submit', (e) => {
       e.preventDefault();
       console.log('Form submitted');
       console.log(input.value);
-      fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=' + input.value)
-         .then((response) => {
-            return response.json();
-         })
-         .then((data) => {
-            let content = '';
-            for (let meal of data.meals) {
-               content += renderItem(meal);
-            }
-            console.log(data);
-            wrapper.innerHTML = content;
-         })
-         .catch(err => {
-            console.error(err);
-            alert('Something went wrong!')
-         })
+
+      showLoading(); //display loading message
+
+      setTimeout(() => {
+         fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=' + input.value)
+            .then((response) => {
+               return response.json();
+            })
+            .then((data) => {
+               let content = '';
+               for (let meal of data.meals) {
+                  content += renderItem(meal);
+               }
+               console.log(data);
+               wrapper.innerHTML = content;
+            })
+            .catch(err => {
+               console.error(err);
+               alert('Something went wrong!')
+            })            
+      }, 2000);
    });
 }
 app();
